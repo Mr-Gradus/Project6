7663a8782aab9e77950690ec26549117cf35402e
Mr-Gradus <rybin.dmitriy@gmail.com>
2022-02-08 13:47:42 +0500

    class pcout

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
-
-
-
+#include <thread>
+#include <mutex>
 
 
 //================================_Task_1_================================
 
+static std::mutex mtx_cout;
+
+class pcout {
+private:
+	std::unique_lock<std::mutex> ulk;
+
+public:
+	pcout() : ulk(std::unique_lock<std::mutex>(mtx_cout)){}
+
+	template<typename T>
+	
+	pcout& operator<<(const T& data)
+	{
+		std::cout << data;
+		return *this;
+	}
+
+	pcout& operator<<(std::ostream& (*fp)(std::ostream&))
+	{
+		std::cout << fp;
+		return *this;
+	}
+};
